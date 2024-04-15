b5f0974c3b22c0375107ada55c3a76602b4bfcdf
SJ <113255841+samjoeld@users.noreply.github.com>
2024-04-15 13:40:41 +0530

    Add files via upload

---
+++ texttomp3.py
@@ -0,0 +1,28 @@
+import os
+import pyttsx3
+
+def text_to_audio(text, filename='output.mp3', save_path='C:\\Users\\tkmra\\Music'):
+    # Initialize the Text-to-Speech engine
+    engine = pyttsx3.init()
+
+    # Set properties (optional)
+    engine.setProperty('rate', 130)  # Speed of speech
+    engine.setProperty('volume', 1)   # Volume (0.0 to 1.0)
+
+    # Select a male voice
+    voices = engine.getProperty('voices')
+    for voice in voices:
+        if "brave" in voice.name.lower():  # Assuming "brave" is part of the male voice's name
+            engine.setProperty('voice', voice.id)
+            break
+
+    # Convert text to speech
+    engine.save_to_file(text, os.path.join(save_path, filename))
+    engine.runAndWait()
+
+# Example text
+text = """
+example text inside hi how are you MR.SJ 
+"""
+
+text_to_audio(text)
